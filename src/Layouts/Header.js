ca37a71ecb724098e07f290849dbb63559f5d7fd
hieuhn <hieungochoangit@gmail.com>
2022-09-07 14:49:20 +0700

    Header UI

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -10,43 +10,43 @@ function Header() {
         return total + product.quantity;
     }, 0);
 
-    const userData = useSelector((state) => state.user);
+    const userData = useSelector(state => state.user);
     const { current, isLogin } = userData;
 
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
     return (
-        <div className="w-full h-15 bg-white shadow-sm shadow-gray-200 fixed top-0 z-20">
+        <div className="w-full h-16 lg:h-20 bg-white shadow-sm shadow-gray-200 fixed top-0 z-20">
             <div className="container px-4 lg:px-14 mx-auto">
-                <div className=" hidden lg:flex">
-                    <div className="flex-none">
-                        <p className="leading-[3.5rem] text-2xl text-amber-300 font-bold text-shadow-xl shadow-yellow-300 ">
+                <div className="hidden lg:flex">
+                    <div >
+                        <p className="leading-[5rem] text-4xl text-amber-300 font-bold text-shadow-xl shadow-yellow-400 ">
                             MStore
                         </p>
                     </div>
                     <div className="flex-1 text-center">
                         <NavLink
-                            className=" leading-[3.5rem] text-xs font-light m-2"
+                            className=" leading-[5rem] text-base font-semibold m-2"
                             to="/"
                         >
-                            Trang chủ
+                            TRANG CHỦ
                         </NavLink>
                         <NavLink
-                            className="leading-[3.5rem] text-xs font-light m-2"
+                            className="leading-[5rem] text-base font-semibold m-2"
                             to="/products"
                         >
-                            Sản phẩm
+                            SẢN PHẨM
                         </NavLink>
                         <NavLink
-                            className="leading-[3.5rem] text-xs font-light m-2"
+                            className="leading-[5rem] text-base font-semibold m-2"
                             to="/contact"
                         >
-                            Liên hệ
+                            LIÊN HỆ
                         </NavLink>
                     </div>
-                    <div className="flex-none">
+                    <div >
                         {isLogin ? (
-                            <div className="inline py-6 text-xs font-light leading-[3.5rem] m-2 group relative">
+                            <div className="inline py-8 text-base font-light leading-[5rem] m-2 group relative">
                                 Hi{' '}
                                 <span className="font-medium">
                                     {current.name}
@@ -58,7 +58,7 @@ function Header() {
                         ) : (
                             <Link
                                 to="/login"
-                                className="text-xs font-light leading-[3.5rem] m-2 hover:cursor-pointer"
+                                className="text-base font-semibold leading-[5rem] m-2 hover:cursor-pointer"
                             >
                                 {/* Đăng nhập */}
 
@@ -66,22 +66,22 @@ function Header() {
                             </Link>
                         )}
 
-                        <p className="inline text-xs leading-[3.5rem] m-2">
+                        <p className="inline text-base leading-[5rem] m-2">
                             <FontAwesomeIcon
-                                className="text-xs leading-[3.5rem]"
+                                className="text-base leading-[5rem]"
                                 icon="fa-solid fa-magnifying-glass"
                             />
                             <input
                                 type="text"
                                 placeholder="Tìm kiếm"
-                                className="text-xs py-1 pl-1 w-2 focus:w-fit font-light placeholder:pl-1 placeholder:text-2xs outline-none"
+                                className="text-base py-1 pl-1 w-2 focus:w-fit font-light placeholder:pl-1 placeholder:text-2xs outline-none"
                             />
                         </p>
 
                         <Link to="/cart">
                             <p className="relative inline">
                                 <FontAwesomeIcon
-                                    className="text-xs leading-[3.5rem]"
+                                    className="text-base leading-[5rem]"
                                     icon="fa-solid fa-cart-plus"
                                 />
                                 <span className="absolute px-1 text-3xs font-medium text-white bg-red-600 -top-1 -right-1.5 rounded-full">
@@ -91,41 +91,41 @@ function Header() {
                         </Link>
                     </div>
                 </div>
-
+ {/* small device header */}
                 <div className="flex justify-between lg:hidden">
                     <div
-                        className="w-1/3 leading-[3.5rem]"
+                        className="w-1/3 leading-[4rem]"
                         onClick={() => setIsSidebarOpen(true)}
                     >
                         <FontAwesomeIcon
-                            className="text-xs leading-[3.5rem]"
+                            className="text-base leading-[4rem]"
                             icon="fa-solid fa-bars"
                         />
                     </div>
 
                     <div className="w-1/3 text-center">
-                        <p className="leading-[3.5rem] text-xl text-amber-300 font-bold text-shadow-xl shadow-yellow-300 ">
+                        <p className="leading-[4rem] text-2xl text-amber-300 font-bold text-shadow-xl shadow-yellow-400 ">
                             MStore
                         </p>
                     </div>
 
                     <div className="w-1/3 text-right">
-                        <p className="inline text-xs leading-[3.5rem] m-2">
+                        <p className="inline text-base leading-[4rem] m-2">
                             <FontAwesomeIcon
-                                className="text-xs leading-[3.5rem]"
+                                className="text-base leading-[4rem]"
                                 icon="fa-solid fa-magnifying-glass"
                             />
                             <input
                                 type="text"
                                 placeholder="Tìm kiếm"
-                                className="text-xs py-1 pl-1 w-2 focus:w-fit font-light placeholder:pl-1 placeholder:text-2xs outline-none"
+                                className="text-base py-1 pl-1 w-2 focus:w-fit font-light placeholder:pl-1 placeholder:text-2xs outline-none"
                             />
                         </p>
 
                         <Link to="/cart">
                             <p className="relative inline">
                                 <FontAwesomeIcon
-                                    className="text-xs leading-[3.5rem]"
+                                    className="text-base leading-[4rem]"
                                     icon="fa-solid fa-cart-plus"
                                 />
                                 <span className="absolute px-1 text-3xs font-medium text-white bg-red-600 -top-1 -right-1.5 rounded-full">
@@ -158,26 +158,26 @@ function Header() {
                                     : 'ease-in -translate-x-full'
                             }`}
                         >
-                            <div className="flex items-center justify-center text-center pt-6">
-                                <span className="mx-2 text-xl font-semibold text-black">
+                            <div className="flex items-center justify-center text-center mt-8">
+                                <span className="mb-2.5 text-xl font-semibold text-black">
                                     MStore
                                 </span>
                             </div>
                             <div className="flex flex-col text-center ">
                                 <NavLink
-                                    className=" text-xs font-light m-2"
+                                    className="text-base font-medium m-1.5"
                                     to="/"
                                 >
                                     Trang chủ
                                 </NavLink>
                                 <NavLink
-                                    className="text-xs font-light m-2"
+                                    className="text-base font-medium m-1.5"
                                     to="/products"
                                 >
                                     Sản phẩm
                                 </NavLink>
                                 <NavLink
-                                    className="text-xs font-light m-2"
+                                    className="text-base font-medium m-1.5"
                                     to="/contact"
                                 >
                                     Liên hệ
@@ -185,7 +185,7 @@ function Header() {
                                 {isLogin ? (
                                     <div
                                         to="/login"
-                                        className="inline text-xs font-light mt-8 group relative"
+                                        className="inline text-base font-medium mt-8 group relative"
                                     >
                                         <span className="font-medium">
                                             {current.name}
@@ -195,7 +195,7 @@ function Header() {
                                 ) : (
                                     <Link
                                         to="/login"
-                                        className="text-xs font-light leading-[3.5rem] m-2 hover:cursor-pointer"
+                                        className="text-base font-medium leading-[4rem] m-2 hover:cursor-pointer"
                                     >
                                         Đăng nhập
                                     </Link>
