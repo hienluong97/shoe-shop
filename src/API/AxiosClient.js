621c28044089462af0efbbb7d710e1e1eaa3e2f0
hienluong <hienluong1997@gmail.com>
2022-12-09 09:38:27 +0700

    add data

--- src/API/AxiosClient.js
+++ src/API/AxiosClient.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
 import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
 
 const getFirebaseToken = async () => {
     const currentUser = firebase.auth().currentUser;
